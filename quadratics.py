c194e564c2d5898bf758213f4cea483f414e877b
Mr Green <dwgreen@hwdsb.on.ca>
2022-02-19 08:40:44 -0500

    fixed coefficients

--- quadratics.py
+++ quadratics.py
@@ -11,11 +11,31 @@ class Quadratic:
         self.b = getCoeff()
         self.c = getCoeff()
 
+    def coeffB(self):
+        if self.b == 0:
+            return ""
+        elif self.b == 1:
+            return f" + x"
+        elif self.b == -1:
+            return f" - x"
+        elif self.b < 0:
+            return f" - {abs(self.b)}x"
+        else:
+            return f" + {self.b}x"
+
+    def coeffC(self):
+        if self.c == 0:
+            return ""
+        elif self.c < 0:
+            return f" - {abs(self.c)}"
+        else:
+            return f" + {self.c}"
+
     def __str__(self):
         if self.a == 1:
-            return f"x\N{SUPERSCRIPT TWO} + {self.b}x + {self.c}"
+            return f"x\N{SUPERSCRIPT TWO}{self.coeffB()}{self.coeffC()}"
         else:
-            return f"{self.a}x\N{SUPERSCRIPT TWO} + {self.b}x + {self.c}"
+            return f"{self.a}x\N{SUPERSCRIPT TWO} + {self.coeffB()}x + {self.coeffC()}"
 
 
 
@@ -27,6 +47,6 @@ class SimpleF(Quadratic):
 
 
 if __name__ == "__main__":
-    for i in range(100):
+    for i in range(10):
         a = SimpleF()
         print(a)
