48a1c4de459df5cbfe8388f3c517e4e4a2668e93
Mr Green <dwgreen@hwdsb.on.ca>
2022-02-20 07:50:50 -0500

    commit

--- quadratics.py
+++ quadratics.py
@@ -6,10 +6,10 @@ def getCoeff():
     return random.randint(-6, 6)
 
 class Quadratic:
-    def __init__(self):
-        self.a = getCoeff()
-        self.b = getCoeff()
-        self.c = getCoeff()
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
     def __str__(self):
         if self.a == 1:
@@ -17,6 +17,46 @@ class Quadratic:
         else:
             return f"{self.a}x\N{SUPERSCRIPT TWO} + {self.b}x + {self.c}"
 
+    def quadraticSF(self):
+        if self.a == 1:
+            return f"x\N{SUPERSCRIPT TWO} + {self.b}x + {self.c}"
+        else:
+            return f"{self.a}x\N{SUPERSCRIPT TWO} + {self.b}x + {self.c}"
+
+    def quadraticVF(self):
+        return "Vertex Form"
+
+    def quadraticFF(self):
+        return "Factored Form"
+
+    def numberOfRoots(self):
+        """
+        Returns the number of roots that a quadratic has (0, 1, 2)
+        """
+        discr = (self.b)**2 - 4*self.a*self.c
+        if discr > 0:
+            return 2
+        elif discr == 0:
+            return 1
+        else:
+            return 0
+
+    def report(self):
+        """
+        Prints information to the screen.
+        Print:
+            Quadratic
+            Vertex
+            Roots
+            y-Intercept
+        """
+        print("Quadratic Report\n")
+        print(f"Standard Form: y = {self.quadraticSF()}")
+        print(f"  Vertex Form: y = {self.quadraticVF()}")
+        print(f"Factored Form: y = {self.quadraticFF()}")
+        print(f"\n  y-Intercept: (0, {self.c})")
+        print(f"   # of Roots: {self.numberOfRoots()}")
+        print(f"       Vertex: Vertex")
 
 
 class SimpleF(Quadratic):
@@ -26,7 +66,10 @@ class SimpleF(Quadratic):
         self.c = getCoeff()
 
 
+
+#*****************************************************************
 if __name__ == "__main__":
-    for i in range(100):
-        a = SimpleF()
-        print(a)
+    testA = Quadratic(1, 5, 6)
+    testB = Quadratic(1, 2, 4)
+    testC = Quadratic(1, 4, 4)
+    testA.report()
