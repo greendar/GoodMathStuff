b27f1869b874ef4f75b0d99dad5b16aa5cb2a82e
MrGreen <dwgreen@hwdsb.on.ca>
2022-03-14 14:29:59 -0400

    created points class

---
+++ points.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distFromOrig(self):
+        return ((self.x**2 + self.y**2)**0.5)
+
+    def __str__(self):
+        return f"({self.x}, {self.y})
+
